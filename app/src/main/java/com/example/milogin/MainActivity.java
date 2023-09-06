f9b6b405520937493f8114546ca25de06abb2be1
pablojlpk <113646557+pablojlpk@users.noreply.github.com>
2023-09-05 21:06:42 -0300

    -

--- app/src/main/java/com/example/milogin/MainActivity.java
+++ app/src/main/java/com/example/milogin/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.milogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.milogin.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-private resultado vm
+
+    private MainActivityViewModel mv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityMainBinding binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mv= ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+
+        binding.btAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+        mv.validarUsuario(binding.ednomusu.getText().toString(), binding.edclave.getText().toString());
+            }
+        });
 
 
 
-
-
-        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/example/milogin/MainActivityViewModel.java
@@ -0,0 +1,46 @@
+package com.example.milogin;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    private Context context;
+    private MutableLiveData<Usuario> usu;
+
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        context = application.getApplicationContext();
+
+    }
+
+    public LiveData<Usuario> getUsuarioIngresado() {
+        if (usu == null) {
+            usu = new MutableLiveData<>();
+            ;
+        }
+        return usu;
+    }
+
+    public void inicializarUsuario() {
+        Usuario usuarioharc = new Usuario("Pablo", "1234");
+    }
+
+    public void validarUsuario(String n, String p) {
+        if (n.length() == 0 && p.length() == 0) {
+            Toast.makeText(context, "falta ingresar el usuario y/o la contraseña", Toast.LENGTH_LONG).show();
+        } else {
+            Intent intent = new Intent(context, MainActivitymenu.class);
+            intent.addFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+
+        }
+    }
+}
